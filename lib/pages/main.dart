9b1910444748513a307dbe07b600285669985ffc
Shafiq Zakaria <shafiqz2004@gmail.com>
2025-12-05 23:30:43 +0800

    fixed the auth folder name

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rechoice_app/auth.dart/login_or_register.dart';
+import 'package:rechoice_app/auth/login_or_register.dart';
 
 void main() {
   runApp(const MainApp());
