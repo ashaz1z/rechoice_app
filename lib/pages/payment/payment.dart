56411c669eb455e5c6521cbec9b1d3dc2248e09d
Shafiq Zakaria <shafiqz2004@gmail.com>
2025-12-23 23:02:53 +0800

    added some loc for payment page

--- lib/pages/payment/payment.dart
+++ lib/pages/payment/payment.dart
@@ -1,1 +1,52 @@
- 
+import 'package:flutter/material.dart';
+
+class Payment extends StatefulWidget {
+  const Payment({super.key});
+
+  @override
+  State<Payment> createState() => _PaymentState();
+}
+
+class _PaymentState extends State<Payment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Payment',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        flexibleSpace: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              colors: [Colors.blue[900]!, Colors.blue[700]!, Colors.blue[500]!],
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            Text(
+              'Payment Details',
+              style: TextStyle(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 10),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[ListTile(enabled: true,)],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
